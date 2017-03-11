9d5c59c0e7f672107970fae2757b479a52058830
shereen <github email address>
2017-03-11 23:24:49 +0300

    Done.

---
+++ js/script.js
@@ -0,0 +1,40 @@
+function GithubInteractor(token){
+	
+	this.token=token;
+
+}
+var gitInteractor=new GithubInteractor('69ccf25137aaf360380aeb95704d235c125eea90')
+function createIssue(repo,owner,title,body){
+	var url="https://api.github.com/repos/"+owner+"/"+repo+"/issues";
+	console.log(url)
+	var data={title: title,body: body}
+	$.ajax({
+		url: url,
+		type: "POST",
+		data: JSON.stringify(data),
+		success: handleResponse,
+		
+		headers : { Authorization: "token "+gitInteractor.token },
+
+	}).fail(handleError)
+}
+function handleResponse(json){
+	$('#issue').html("<a href='"+json.html_url+"'>"+json.title+"</a>")
+}
+
+function handleError(xhr,status,error){
+	console.log("Post error: " + error);
+}
+function submitForm(){
+	$('form').submit(function(e){
+		e.preventDefault();
+		var repo=$('#repoName').val();
+		var owner=$('#repoOwner').val();
+		var title=$('#title').val();
+		var body=$('#body').val();
+		createIssue(repo,owner,title,body)
+	})
+}
+$(document).ready(function(){
+	submitForm();
+})
